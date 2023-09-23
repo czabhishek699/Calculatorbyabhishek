f049430e6f7f9c6126364f9585ac81172c89bcdd
AVISHEK OJHA <140743024+czabhishek699@users.noreply.github.com>
2023-09-23 20:09:00 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+let output = document.getElementById("output");
+
+function appendToOutput(value) {
+    output.textContent += value;
+}
+
+function calculate() {
+    if (output.textContent === "1+1") {
+        output.textContent = "I LOVE YOU JONY MY FRIEND";
+    } else {
+        
+        try {
+            const result = eval(output.textContent);
+            output.textContent = result;
+        } catch (error) {
+            output.textContent = "Error";
+        }
+    }
+}function deleteLastCharacter() {
+    let currentOutput = output.textContent;
+    if (currentOutput.length > 0) {
+        output.textContent = currentOutput.slice(0, -1); 
+    }
+}
