73d29b152bf434762349d3cc0ad9b3b1cb7943bf
AVISHEK OJHA <140743024+czabhishek699@users.noreply.github.com>
2023-11-22 12:09:10 +0545

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function appendToOutput(value) {
 
 function calculate() {
     if (output.textContent === "1+1") {
-        output.textContent = "Its obviously 2 my friend";
+        output.textContent = "Happy Birthday Pralika❤️";
     } else {
         
         try {
