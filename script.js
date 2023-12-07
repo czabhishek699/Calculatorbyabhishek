396539c7b112156a1664c73e26c0e98a05952dd8
AVISHEK OJHA <140743024+czabhishek699@users.noreply.github.com>
2023-12-07 20:20:47 +0545

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function appendToOutput(value) {
 
 function calculate() {
     if (output.textContent === "1+1") {
-        output.textContent = "It's obviously 2 yrr";
+        output.textContent = "Dipesh mula k xa khabar";
     } else {
         
         try {
